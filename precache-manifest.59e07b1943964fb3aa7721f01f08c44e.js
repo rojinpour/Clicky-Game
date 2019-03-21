27e4d56478677e191145cf85fd7c072a7c856f3e
Rojin Pourkhomami <Rojin@Rojins-MacBook-Pro.local>
2019-03-21 18:10:24 -0400

    Updates

---
+++ precache-manifest.59e07b1943964fb3aa7721f01f08c44e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeed53eec4392fa9b2da",
+    "url": "/Clicky-Game/static/css/main.30604398.chunk.css"
+  },
+  {
+    "revision": "eeed53eec4392fa9b2da",
+    "url": "/Clicky-Game/static/js/main.15a23028.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/Clicky-Game/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d02e335f66c502402bb909e30c2a965",
+    "url": "/Clicky-Game/index.html"
+  }
+];
